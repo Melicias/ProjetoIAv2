c8b4e146b18e3bc0857d003b0f746a09d8dea3a8
Francisco Melicias <franciscomelicias@MBPdeFrancisco.lan>
2021-05-06 19:25:38 +0100

    recombination2 a funcionar

--- src/ga/geneticoperators/Recombination2.java
+++ src/ga/geneticoperators/Recombination2.java
@@ -20,23 +20,26 @@ public class Recombination2<I extends IntVectorIndividual, P extends Problem<I>>
         child1 = new int[ind1.getNumGenes()];
         child2 = new int[ind2.getNumGenes()];
 
-        int value1 = ind1.getGene(0);
-        child1[0] = value1;
+        for (int i = 0; i < ind1.getNumGenes(); i++) {
+            child1[i]=-1;
+            child2[i]=-1;
+        }
+
+        child1[0] = ind1.getGene(0);
         int value2 = ind2.getGene(0);
         child2[0] = value2;
-
+        
         do {
             child1[ind1.getIndexof(value2)] = value2;
-            child2[ind2.getIndexof(value1)] = value1;
-            value1 = ind1.getGene(ind2.getIndexof(value1));
             value2 = ind2.getGene(ind1.getIndexof(value2));
-        } while (child1[ind1.getIndexof(value2)] != value2);
-
+            child2[ind2.getIndexof(value2)] = value2;
+            System.out.println("child1[0]=" + child1[0] + "    - " + value2);
+        } while (child1[0] != value2);
 
         for(int i = 0;i<child1.length;i++){
-            if(child1[i] == 0)
+            if(child1[i] == -1)
                 child1[i] = ind2.getGene(i);
-            if(child2[i] == 0)
+            if(child2[i] == -1)
                 child2[i] = ind1.getGene(i);
         }
 
@@ -45,14 +48,11 @@ public class Recombination2<I extends IntVectorIndividual, P extends Problem<I>>
             ind2.setGene(i, child2[i]);
         }
 
-        System.out.println(Arrays.toString(child1));
-        System.out.println(Arrays.toString(child2));
-        System.out.println(Arrays.toString(ind1.getGenome()));
     }
 
     @Override
     public String toString(){
         //TODO
-        return "CX";
+        return "CX cycle crossover";
     }
 }
